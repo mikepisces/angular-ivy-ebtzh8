4fa02476c07b248446ec6b745a453d832e47037b
mikepisces <mukeshprasad121@gmail.com>
2020-07-19 01:01:55 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {CollegeService} from './services/college.service';
 import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -11,7 +12,7 @@ import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule,HttpClientModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AdminComponent, HomeComponent ],
 
   providers : [CollegeService],
 
