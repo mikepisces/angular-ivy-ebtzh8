b7161a0917ebd242e3d4df63a444bef6da57e727
mikepisces <mukeshprasad121@gmail.com>
2020-07-18 23:20:24 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {CollegeService} from './services/college.service';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule,HttpClientModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
+
+  providers : [CollegeService],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
