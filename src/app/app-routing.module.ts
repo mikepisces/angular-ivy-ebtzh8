deac2e1a6117e45c0e01411d0761713467144f63
mikepisces <mukeshprasad121@gmail.com>
2020-07-19 00:56:02 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,28 +3,21 @@ import { CollegeService} from '../../services/college.service';
 import {Observable} from 'rxjs';
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 
 
-export class AdminComponent implements OnInit {
+export class HomeComponent implements OnInit {
    
-   public students;
+   
    constructor(private collegeService: CollegeService){   }
    ngOnInit(){
-      this.getStudentDetails();
+      
    }
 
-   getStudentDetails(){
-
-     this.collegeService.getStudentDetails().subscribe(
-        data => {this.students = data },
-        err => console.error(err),
-        () => console.log('students loaded')
-     );
-   }
+  
 
    
 
