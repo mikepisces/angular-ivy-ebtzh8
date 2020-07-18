cba536069ddaf1a420b97dfde1a3650652a13830
mikepisces <mukeshprasad121@gmail.com>
2020-07-18 23:27:55 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {CollegeService} from './CollegeService';
+import {CollegeService} from './services/college.service';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
