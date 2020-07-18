c41dd53d2f39007d0d41cf412ef549e05fa51545
mikepisces <mukeshprasad121@gmail.com>
2020-07-19 00:59:26 +0530

    1 file modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CollegeService} from '../../services/college.service';
 import {Observable} from 'rxjs';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +12,16 @@ import {Observable} from 'rxjs';
 
 export class HomeComponent implements OnInit {
    
-   
+   studentform: FormGroup;
+   validMessage: string="";
+
    constructor(private collegeService: CollegeService){   }
    ngOnInit(){
-      
+      this.studentform = new FormGroup({
+        name: new FormControl('', Validators.required),
+        email:  new FormControl('', Validators.required),
+        phone:  new FormControl('', Validators.required)
+      })
    }
 
   
